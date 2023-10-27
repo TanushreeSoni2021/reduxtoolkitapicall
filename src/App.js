e91b314f2d50f6ca5471726b07bf37e7f69e5299
Tanushree Soni <108271833+TanushreeSoni2021@users.noreply.github.com>
2023-10-27 15:43:28 +0530

    complete the project of api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from "./redux/slice/todo";
+
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  console.log("state", state);
+  if (state.todo.isLoading) {
+    return <h1>Loading......</h1>;
+  }
   return (
     <>
-      <div>hey app</div>
+      <button onClick={(e) => dispatch(fetchTodos())}>Fetch todos</button>
+      {state.todo.data &&
+        state.todo.data.map((e) => (
+          <li>
+            {e.id}. {e.title}
+          </li>
+        ))}
     </>
   );
 }
--- src/redux/slice/todo.js
+++ src/redux/slice/todo.js
@@ -1,8 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// thunk for delayed task like api call
 
-const  todoSlice = createSlice({
-    name: 'todo',
+//Action
+export const fetchTodos = createAsyncThunk("fetchTodos", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return response.json();
+});
 
-
-})
+//extraReducer is basically a function in which we have builders by this we can listen to the fetchTodos
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    isLoading: false,
+    data: null,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchTodos.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchTodos.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchTodos.rejected, (state, action) => {
+      console.log("Error", action.payload);
+      state.isError = true;
+    });
+  },
+});
 export default todoSlice.reducer;
